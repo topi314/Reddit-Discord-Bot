ecf82deec6a832388417a7065601a3a8a3b02146
TopiSenpai <tobi@strasserhof.com>
2022-11-12 12:16:37 +0100

    increase stream interval to 10 minutes

--- subreddit_manager.go
+++ subreddit_manager.go
@@ -67,7 +67,7 @@ func (b *RedditBot) unsubscribeFromSubreddit(subreddit string, webhookID snowfla
 
 func (b *RedditBot) listenToSubreddit(subreddit string, ctx context.Context) {
 	b.Logger.Debugf("listening to r/%s", subreddit)
-	posts, errs, closer := b.RedditClient.Stream.Posts(subreddit, reddit.StreamInterval(time.Minute*5), reddit.StreamDiscardInitial)
+	posts, errs, closer := b.RedditClient.Stream.Posts(subreddit, reddit.StreamInterval(time.Minute*10), reddit.StreamDiscardInitial)
 	defer closer()
 	defer b.Logger.Debugf("stop listening to r/%s", subreddit)
 	for {
